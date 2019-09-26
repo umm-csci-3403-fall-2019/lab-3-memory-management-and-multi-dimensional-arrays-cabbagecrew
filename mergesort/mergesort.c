099bbb6cc90134ec969dae227d8757236411b1de
michael-small <small203@morris.umn.edu>
2019-09-26 11:29:54 -0500

    add java implimentation of mergesort algorithm in c code. not yet complete

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,41 @@
 #include "mergesort.h"
 
 void mergeRanges(int* values, int startIndex, int midPoint, int endIndex){
-  int rangeSize;
-  int* destination;
-  int firstIndex;
-  int secondIndex;
-  int copyIndex = 0;
-  return;
+
+    int rangeSize = endIndex - startIndex;
+    int* destination = (int)calloc(rangeSize, sizeof(int)); 
+    int firstIndex = startIndex;
+    int secondIndex = midPoint;
+    int copyIndex = 0;
+    
+    while (firstIndex < midPoint && secondIndex < endIndex) {
+      if (values[firstIndex] < values[secondIndex]) {
+        destination[copyIndex] = values[firstIndex];
+        ++firstIndex;
+      } else {
+        destination[copyIndex] = values[secondIndex];
+        ++secondIndex;
+      }
+      ++copyIndex;
+    }
+    while (firstIndex < midPoint) {
+      destination[copyIndex] = values[firstIndex];
+      ++copyIndex;
+      ++firstIndex;
+    }
+    while (secondIndex < endIndex) {
+      destination[copyIndex] = values[secondIndex];
+      ++copyIndex;
+      ++secondIndex;
+    }
+    for (int i = 0; i < rangeSize; ++i) {
+      values[i + startIndex] = destination[i];
+    }
+  }
+ }
+
+boolean needsSorting(int rangeSize){
+return rangeSize >= 2;
 }
 
 void mergesortRange(int* values, int startIndex, int endIndex) {
@@ -18,3 +47,6 @@ void mergesort(int size, int* values) {
   return;
 }
 
+void main(int argc) {
+  printf("Main runs");
+}
