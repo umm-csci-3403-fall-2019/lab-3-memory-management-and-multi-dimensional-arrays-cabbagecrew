dc4755562bdde5d52adb3b3b70d4c566383ef257
michael-small <small203@morris.umn.edu>
2019-09-29 20:13:24 -0500

    Finish mergesort.c

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,8 +1,10 @@
 #include "mergesort.h"
+#include <cstdlib>
+#include <cstdio>
 
 void mergeRanges(int* values, int startIndex, int midPoint, int endIndex){
     int rangeSize = endIndex - startIndex;
-    int* destination = (int)calloc(rangeSize, sizeof(int)); 
+    int* destination = (int*)calloc(rangeSize, sizeof(int)); 
     int firstIndex = startIndex;
     int secondIndex = midPoint;
     int copyIndex = 0;
@@ -10,42 +12,44 @@ void mergeRanges(int* values, int startIndex, int midPoint, int endIndex){
     while (firstIndex < midPoint && secondIndex < endIndex) {
       if (values[firstIndex] < values[secondIndex]) {
         destination[copyIndex] = values[firstIndex];
-        ++firstIndex;
+        firstIndex++;
       } else {
         destination[copyIndex] = values[secondIndex];
-        ++secondIndex;
+        secondIndex++;
       }
-      ++copyIndex;
+      copyIndex++;
     }
     while (firstIndex < midPoint) {
       destination[copyIndex] = values[firstIndex];
-      ++copyIndex;
-      ++firstIndex;
+      copyIndex++;
+      firstIndex++;
     }
     while (secondIndex < endIndex) {
       destination[copyIndex] = values[secondIndex];
-      ++copyIndex;
-      ++secondIndex;
+      copyIndex++;
+      secondIndex++;
     }
-    for (int i = 0; i < rangeSize; ++i) {
+    for (int i = 0; i < rangeSize; i++) {
       values[i + startIndex] = destination[i];
     }
-  }
 }
 
-boolean needsSorting(int rangeSize){
+bool needsSorting(int rangeSize){
     return rangeSize >= 2;
 }
 
 void mergesortRange(int* values, int startIndex, int endIndex) {
-    int rangeSize;
-    return;
+    int rangeSize = endIndex - startIndex;
+    if (needsSorting(rangeSize)) {
+        int midPoint = (startIndex + endIndex) / 2;
+        mergesortRange(values, startIndex, midPoint);
+        mergesortRange(values, midPoint, endIndex);
+        mergeRanges(values, startIndex, midPoint, endIndex);
+    }
 }
 
 void mergesort(int size, int* values) {
-    return;
+    mergesortRange(values, 0, size);
 }
 
-void main(int argc) {
-    printf("Main runs");
-}
+
