86e619300ffc86c17bc1fc4ee730ab6906f11816
michael-small <small203@morris.umn.edu>
2019-10-05 20:23:52 -0500

    Describe our 5 step process to proceed with array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -3,6 +3,18 @@
 #include "../mergesort/mergesort.h"
 #include <stdio.h>
 
+//Our idea for steps to approach this issue:
+
+//1: Combine all `values` into a single array, including duplicates
+//2: Sort the single array using `mergesort`
+//3: Use the most succinct unique counting approach that we found: gevni's counter 
+//     @ https://cboard.cprogramming.com/c-programming/155200-how-can-i-count-total-no-unique-elements-array.html#post1154407
+//     Our modification of this involves creating another array in the next step...
+//4: Create an array for the unique values. As #3 adds to the unique counter, it will also put in that unique value into 
+//     our new array. This will add them uniquely in order. 
+//5: Put the unique counter at the head of the array. Not sure if we need to reserve the spot for it upfront or if it could
+//     just but moved into the front. 
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
   return sizes;
